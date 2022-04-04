7aca637160b3327f36bd2281e4046983ac3e4112
Dima <ivashindev@gmail.com>
2022-04-04 16:41:16 +0200

    Initial Commit

---
+++ ScotiabankCodingAssessment/ScotiabankCodingAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScotiabankCodingAssessmentApp.swift
+//  ScotiabankCodingAssessment
+//
+//  Created by Dmitry Ivashin on 04.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScotiabankCodingAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
